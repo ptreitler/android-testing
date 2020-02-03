c04917db74ccf1f76dd65404a5b23e6bd6e3565a
Peter Treitler <peter.treitler@gmail.com>
2020-02-03 22:44:50 +0100

    Add tests for getActiveAndCompletedStats

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,5 +1,30 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
+import com.example.android.architecture.blueprints.todoapp.data.Task
 import org.junit.Assert.*
+import org.junit.Test
 
-class StatisticsUtilsTest
+class StatisticsUtilsTest {
+    @Test
+    fun getActiveAndCompletedTasksWithOnlyOneActiveTask() {
+        val tasks = listOf(Task(
+                "Active task",
+                "An active task for the unit test",
+                isCompleted = false
+        ))
+        val (activeTasksPercent, completedTasksPercent) = getActiveAndCompletedStats(tasks)
+        assertEquals(100f, activeTasksPercent)
+        assertEquals(0f, completedTasksPercent)
+    }
+
+    @Test
+    fun getActiveAndCompletedTasksWithTwoOutOfThreeTasksCompleted() {
+        val tasks = listOf(Task("A", "A", true),
+                Task("B", "B", true),
+                Task("C", "C", false)
+        )
+        val (activeTasksPercent, completedTasksPercent) = getActiveAndCompletedStats(tasks)
+        assertEquals(33.33f, activeTasksPercent, 0.01f)
+        assertEquals(66.66f, completedTasksPercent, 0.01f)
+    }
+}
