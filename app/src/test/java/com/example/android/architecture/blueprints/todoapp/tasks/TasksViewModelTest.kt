3b08cfc73da8c68fdb3c56d0fb8b2dcda1f15fef
Peter Treitler <peter.treitler@gmail.com>
2020-02-12 20:06:27 +0100

    Add a test that checks whether the 'add task' action is visible

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -7,6 +7,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.Event
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,10 +17,16 @@ class TasksViewModelTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var tasksViewModel: TasksViewModel
+
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
         // Given a fresh view model
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
         // when adding a new task
         tasksViewModel.addNewTask()
@@ -27,5 +34,17 @@ class TasksViewModelTest {
         // then the new task event is triggered
         val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
         assertThat(value.getContentIfNotHandled()).isNotNull()
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+        // Given a fresh ViewModel
+
+        // When the filter type is ALL_TASKS
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        // Then the "Add task" action is visible
+        val tasksAddViewVisible = tasksViewModel.tasksAddViewVisible.getOrAwaitValue()
+        assertThat(tasksAddViewVisible).isTrue()
     }
 }
