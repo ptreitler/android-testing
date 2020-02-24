da476f182749976a0c526872457ee143fb5d102d
Peter Treitler <peter.treitler@gmail.com>
2020-02-24 23:51:13 +0100

    Add simple Espresso tests to TaskDetailFragmentTest

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -1,6 +1,9 @@
 package com.example.android.architecture.blueprints.todoapp.taskdetail
 
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.example.android.architecture.blueprints.todoapp.R
@@ -33,14 +36,29 @@ class TaskDetailFragmentTest {
     }
 
     @Test
-    fun activeTaskDetails_displayedInUI() = runBlockingTest {
-        // Given: Add active (incomplete) task to the DB
-        val activeTask = Task("Active task", "An active test task", false)
+    fun activeTaskDetails_displayedInUI() {
+        val title = "Active task"
+        val description = "An active test task"
+        val activeTask = Task(title, description, false)
 
-        repository.saveTask(activeTask)
+        runBlockingTest {
+            // Given: Add active (incomplete) task to the DB
 
-        // When: Details Fragment launched to display task
-        val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
-        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+            repository.saveTask(activeTask)
+
+            // When: Details Fragment launched to display task
+            val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
+            launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+
+            // Then: Task details are displayed on the screen
+            // make sure that the title/description are both shown and correct
+            onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
+            onView(withId(R.id.task_detail_title_text)).check(matches(withText(title)))
+            onView(withId(R.id.task_detail_description_text)).check(matches(isDisplayed()))
+            onView(withId(R.id.task_detail_description_text)).check(matches(withText(description)))
+            // and make sure the "active" checkbox is shown unchecked
+            onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isDisplayed()))
+            onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isNotChecked()))
+        }
     }
 }
