e18d2b9dcd4effa8e2762219a63935a29fe4eb4b
Peter Treitler <peter.treitler@gmail.com>
2020-03-02 22:39:31 +0100

    Add StatisticsViewModelTest

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
@@ -0,0 +1,45 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class StatisticsViewModelTest {
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    // Subject under test
+    private lateinit var statisticsViewModel: StatisticsViewModel
+
+    private lateinit var repository: FakeTestRepository
+
+    @Before
+    fun setUp() {
+        repository = FakeTestRepository()
+        statisticsViewModel = StatisticsViewModel(repository)
+    }
+
+    @Test
+    fun loadTasks_loading() {
+        mainCoroutineRule.pauseDispatcher()
+        statisticsViewModel.refresh()
+
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue()).isTrue()
+        mainCoroutineRule.resumeDispatcher()
+
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue()).isFalse()
+    }
+}
